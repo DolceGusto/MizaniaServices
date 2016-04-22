1429cdffd652768ea5b7362ac940610a98b1888c
wissemCherouana <cw_cherouana@esi.dz>
2016-04-23 00:30:40 +0100

    Controllers Javascript

---
+++ UnityTest_v1/ControllerJS/CategorieBDController.js
@@ -0,0 +1,54 @@
+﻿(function () {
+    var CategorieBDController = function ($scope, categoriesService, $log, $routeParams) {
+        var Categories = function (data) {
+            $scope.Categories = data;
+        };
+
+        var singleCategorie = function (data) {
+            $scope.existingCategorie = data;
+            $log.info(data);
+        };
+
+
+        $scope.init = function () {
+            categoriesService.singleCategorie($routeParams.id)
+                .then(singleCategorie, errorDetails);
+        };
+
+        var categorie = {
+            id: null,
+            idPorteFeuille: 1,
+            desination: null,
+            descript : null
+        };
+        $scope.categorie = categorie;
+
+        categoriesService.Categories().then(Categories, errorDetails);
+
+        var errorDetails = function (serviceResp) {
+            $scope.Error = "Something went wrong ??";
+        };
+
+        $scope.InsertCategorie = function (categorie) {
+            categoriesService.InsertCategorie(categorie).then(categoriesService.Categories().then(Categories, errorDetails));
+
+        };
+
+        $scope.ModifyCategorie = function (existingCategorie) {
+            $log.info(existingCatgorie);
+            categoriesService.ModifyCategorie(existingCategorie).then(categoriesService.Categories().then(Categories, errorDetails));
+        };
+
+        $scope.deleteCategorie = function (categorie) {
+            $log.info(categorie);
+            if (confirm("Etes-vous sûr de vouloir supprimer cette catégorie ?")) {
+                categoriesService.deleteCategorie(categorie)
+                    .then(Categories, errorDetails);
+            }
+        };
+
+
+      
+    };
+    app.controller("CategorieBDController", ["$scope", "categoriesService", "$log", "$routeParams", CategorieBDController]);
+}());
---
+++ UnityTest_v1/ControllerJS/UserBDController.js
@@ -0,0 +1,77 @@
+﻿(function () {
+    var UserBDController = function ($scope, usersService, $log, $routeParams) {
+        var Users = function (data) {
+            $scope.Users = data;
+        };
+
+        var singleUser = function (data) {
+            $scope.existingUser = data;
+            $log.info(data);
+        };
+
+
+        $scope.init = function () {
+            usersService.singleUser($routeParams.id)
+                .then(singleUser, errorDetails);
+        };
+
+        $scope.SearchUsers = function (UserName) {
+            usersService.SearchUsers(UserName)
+            .then(Users, errorDetails);
+            $log.info('Found user which contains - ' + UserName);
+        };
+
+
+
+
+        var user = {
+            id: null,
+            nom: null,
+            prenom: null,
+            nomDeCompte: null,
+            roleUtilisateur: null,
+            idPorteFeuille: null
+        };
+        $scope.user = user;
+
+        usersService.Users().then(Users, errorDetails);
+
+        var errorDetails = function (serviceResp) {
+            $scope.Error = "Something went wrong ??";
+        };
+
+        $scope.InsertUser = function (user) {
+            usersService.InsertUser(user).then(usersService.Users().then(Users, errorDetails));
+
+        };
+
+        $scope.ModifyUser = function (existingUser) {
+            $log.info(existingUser);
+            usersService.ModifyUser(existingUser).then(usersService.Users().then(Users, errorDetails));
+        };
+
+        $scope.deleteUser = function (user) {
+            $log.info(user);
+            if (confirm("Etes-vous sûr de vouloir supprimer cet utilisateur ?"))
+                 {
+            usersService.deleteUser(user)
+                .then(Users, errorDetails);}
+        };
+
+
+        //  var UserAccounts =function (data) {
+
+
+        $scope.UserAccounts = function (user) {
+            $log.info(user);
+            usersService.UserAccounts(user).then(usersService.Users().then(Users, errorDetails));
+
+        };
+        // };
+
+
+        $scope.Title = "Users";
+        $scope.UserName = null;
+    };
+    app.controller("UserBDController", ["$scope", "usersService", "$log", "$routeParams", UserBDController]);
+}());
