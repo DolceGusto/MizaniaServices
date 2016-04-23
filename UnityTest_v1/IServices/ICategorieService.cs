28b02e4d0fb81a3bc7da5a28c4a093637294ffe7
wissemCherouana <cw_cherouana@esi.dz>
2016-04-23 01:14:10 +0100

    IServices Folder

---
+++ UnityTest_v1/IServices/ICategorieService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityTest_v1.Models; 
+
+namespace UnityTest_v1.IServices
+{
+    interface ICategorieService
+    {
+        Categorie GetCategorieById(int CategoryId);
+        IEnumerable<Categorie> GetAllCategories();
+        bool AddCategorie(Categorie categorie);
+        bool DeleteCategorie(int id);
+        bool UpdateCategorie(Categorie categorie);
+        
+    }
+}
