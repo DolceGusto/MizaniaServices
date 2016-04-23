403f63a87d7be23773996f70e872d022b598fb29
wissemCherouana <cw_cherouana@esi.dz>
2016-04-23 01:36:09 +0100

    ServicesJS

---
+++ UnityTest_v1/ServicesJS/CategoriesService.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    var categoriesService = function ($http, $q, $log) {
+
+
+        var Categories = function () {
+            return $http.get("http://localhost:26309/api/Categorie/getAll/")
+                        .then(function (serviceResp) {
+                            return serviceResp.data;
+                        });
+        };
+
+       
+
+
+        var InsertCategorie = function (categorie) {
+            return $http.post("http://localhost:26309/api/Categorie/addCategorie", categorie)
+            .then(function () {
+                $log.info("Insert Successful");
+                return;
+            });
+        };
+
+        var singleCategorie = function (id) {
+            return $http.get("http://localhost:26309/api/Categorie/getCategorieById/" + id)
+                        .then(function (serviceResp) {
+                            return serviceResp.data;
+                        });
+        };
+
+        var ModifyCategorie = function (categorie) {
+            return $http.put("http://localhost:26309/api/Categorie/updateCategorie/" + categorie.id, categorie)
+            .then(function (result) {
+                $log.info("Update Successful");
+                return;
+            });
+        };
+
+      
+
+        var deleteCategorie = function (categorie) {
+            return $http.delete("http://localhost:26309/api/Categorie/deleteCategorie/" + categorie.id)
+            .then(function (result) {
+                $log.info("Delete Successful");
+                return;
+            });
+        };
+
+        return {
+            Categories: Categories,
+            singleCategorie: singleCategorie,
+            InsertCategorie: InsertCategorie,
+            ModifyCategorie: ModifyCategorie,
+            deleteCategorie: deleteCategorie
+        };
+    };
+    var module = angular.module("MizaniaProjectModule");
+    module.factory("categoriesService", ["$http", "$q", "$log", categoriesService]);
+}());
---
+++ UnityTest_v1/ServicesJS/accountsService.js
@@ -0,0 +1,69 @@
+﻿(function () {
+    var accountsService = function ($http, $q, $log) {
+
+
+        var Accounts = function () {
+            return $http.get("http://localhost:26309/api/Compte/getAll/")
+                        .then(function (serviceResp) {
+                            return serviceResp.data;
+                        });
+        };
+
+        var Users = function () {
+           
+            return $http.get("http://localhost:26309/api/User/getAll/")
+                        .then(function (serviceResp) {
+                            return serviceResp.data;
+                        });
+        };
+
+     
+
+        var InsertAccount = function (user, account) {
+            var strFinal = "[" + JSON.stringify(user) + "," +
+                       JSON.stringify(account) + "]";
+           
+            return $http.post("http://localhost:26309/api/Compte/addUserV", strFinal)
+            .then(function () {
+                $log.info("Insert Successful");
+                return;
+            });
+        };
+
+        var singleAccount = function (id) {
+            return $http.get("http://localhost:26309/api/Compte/getAccountById/" + id)
+                        .then(function (serviceResp) {
+                            return serviceResp.data;
+                        });
+        };
+
+        var ModifyAccount = function (account) {
+            return $http.put("http://localhost:26309/api/Compte/updateAccount/" + account.id, account)
+            .then(function (result) {
+                $log.info("Update Successful");
+                return;
+            });
+        };
+
+       
+        var deleteAccount = function (account) {
+            return $http.delete("http://localhost:26309/api/Compte/deleteAccount/" + account.id)
+            .then(function (result) {
+                $log.info("Delete Successful");
+                return;
+            });
+        };
+
+        
+        return {
+            Accounts: Accounts,
+            Users : Users,
+            singleAccount: singleAccount,
+            InsertAccount: InsertAccount,
+            ModifyAccount: ModifyAccount,
+            deleteAccount: deleteAccount
+        };
+    };
+    var module = angular.module("MizaniaProjectModule");
+    module.factory("accountsService", ["$http", "$q", "$log", accountsService]);
+}());
